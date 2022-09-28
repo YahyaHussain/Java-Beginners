ed72efe425e28e567693b1cf05fcc25b455b9c1b
YahyaHussain <yahyahussain2904@gmail.com>
2022-09-28 12:44:47 +0530

    Add Problem8,9.java

---
+++ Java/Problem8.java
@@ -0,0 +1,36 @@
+/*
+Remove duplicates in a string:
+I/P: abbcccdda
+O/P: abcd
+*/
+import java.util.Scanner;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+        removeDuplicates(text, "", 0);
+
+        System.out.println();
+        sc.close();
+    }
+    public static boolean[] alphaInArray = new boolean[26];
+    public static void removeDuplicates(String text, String result, int index) {
+        if (index == text.length()) {
+            System.out.println(result);
+            return;
+        }
+        char currChar = text.charAt(index);
+        if (alphaInArray[currChar - 'a'])   {
+            removeDuplicates(text, result, index + 1);
+        }
+        else    {
+            result += currChar;
+            alphaInArray[currChar - 'a'] = true;
+            removeDuplicates(text, result, index + 1);
+        }
+    }
+}
---
+++ Java/Problem9.java
@@ -0,0 +1,40 @@
+/*
+Print all sub-sequences in a string:
+I/P: abc
+O/P: abc
+     ab
+     ac
+     a
+     bc
+     b
+     c
+*/
+
+import java.util.Scanner;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+        System.out.print("Enter a string: ");
+        String sequence = sc.nextLine();
+        subsequence(sequence, "", 0);
+
+        System.out.println();
+        sc.close();
+    }
+    public static int count = 1;
+    public static void subsequence(String sequence, String result, int index) {
+        if (index == sequence.length()) {
+            System.out.println(count + " " +  result);
+            count++;
+            return;
+        }
+        char currChar = sequence.charAt(index);
+        // currChar to be in the sub-sequence
+        subsequence(sequence, result + currChar, index + 1);
+        // currChar not to be in the sub-sequence
+        subsequence(sequence, result , index + 1);
+    }
+}
